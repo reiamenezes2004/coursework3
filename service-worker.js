db5b71c564c4f800b8d999f9a100965488b6d5d2
Reia Menezes <RM1661@live.mdx.ac.uk>
2024-04-02 10:35:12 +0400

    new

--- service-worker.js
+++ service-worker.js
@@ -21,26 +21,26 @@ var cacheFiles = [
     'images/theatre.JPG'
 ];
 
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil(    
-        caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all the files');
-            return cache.addAll(cacheFiles);
-    })
-);
- });
-
-self.addEventListener('fetch', function (e){
-    e.respondWith(
-        caches.match(e.request).then(function (r){
-            // console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r || fetch(e.request).then(function(response){
-                return caches.open(cacheName).then(function (cache){
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        return Promise.all(cacheFiles.map(function(url) {
+          return fetch(url).then(function(response) {
+            if (!response.ok) {
+              throw new Error('Failed to fetch resource: ' + url);
+            }
+            return cache.put(url, response);
+          });
+        }));
+      })
     );
-});
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
